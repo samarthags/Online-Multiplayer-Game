1828df63e5dcba287b6ca849ba6b04503420240f
Samartha Gs <119740787+samarthags@users.noreply.github.com>
2026-05-05 16:42:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-/////////////////////////////////////////////////////////////////
-//This fixed the issue with long disconnecting times in browsers
-//The interval checks if player is connected every 1 seconds
-//If the player is disconnected for 5 second, they get booted
-/////////////////////////////////////////////////////////////////
+
 io.set('heartbeat interval', 1000);
 io.set('heartbeat timeout', 5000);
 
